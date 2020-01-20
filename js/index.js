ef87ebe272f6baadbde021448bbcbc1e546f75b7
Dzhafar <djafarrn@gmail.com>
2020-01-20 21:32:23 +0300

    feat: add styles and links

---
+++ js/index.js
@@ -0,0 +1,5 @@
+//import M from './js/materialize.min.js';
+document.addEventListener('DOMContentLoaded', function() {
+    const elems = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(elems);
+});
